a7e8edf7c8aa270586faf9d32d9e909e7e28a419
ElianMartinez <elianmartinez157@gmail.com>
2020-05-25 12:02:06 -0400

    ruta product category

--- src/routes/product_category.js
+++ src/routes/product_category.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const mysqlConnection = require("../DB/database");
 
-router.get("/", (req, res) => {
-  mysqlConnection.query("select * from employe", (err, rows, fields) => {
+router.get("/product_category", (req, res) => {
+  mysqlConnection.query("select * from employe limit 100", (err, rows, fields) => {
     if (!err) {
       res.json(rows);
     } else {
